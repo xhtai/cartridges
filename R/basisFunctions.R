49e9096b97304a2b39d5e39d514901b4df952130
xhtai <tai.xiaohui@gmail.com>
2016-05-10 22:16:10 -0400

    initial commit

---
+++ R/basisFunctions.R
@@ -0,0 +1,150 @@
+#' Get ij coordinates for circularly symmetric basis functions
+#'
+#' A circularly symmetric image has the same pixel values
+#' for pixels that are the same distance from the center.
+#' A circularly symmetric basis spans the space of circularly
+#' symmetric matrices. Each matrix in the basis takes the
+#' value 1 for pixels that are the same distance from the
+#' center, and zero otherwise. Basis are enumerated from
+#' center outwards.
+#'
+#' For a 3 x 3 matrix, there are a total of 3 basis. The
+#' first has 1 for the (2, 2) pixel and 0 otherwise. The
+#' second basis has 1's for coordinates (1, 2), (2, 1),
+#' (2, 3) and (3, 2), and 0 otherwise. The third has
+#' 1's for coordinates (1, 1), (1, 3), (3, 1), (3, 3),
+#' and 0's otherwise. Functions that take an ij coordinate
+#' as an input and return the value 0 or 1 are termed
+#' circularly symmetric basis functions.
+#'
+#' @param dimension size of image (\code{dimension} x
+#' \code{dimension}), where \code{dimension} is odd
+#'
+#' @return A list with length = number of basis functions
+#' for an image of size \code{dimension} x \code{dimension}.
+#' Each list item is a matrix whose rows are the ij
+#' coordinates of pixels in each basis function, and i2j2
+#' is the distance from the center of the pixels in each
+#' basis.
+#' @examples
+#' basis3 <- getBasisFunctions(3)
+#'
+
+getBasisFunctions<-function(dimension){
+    m<-dimension
+    n<-(m-1)/2
+    values<-expand.grid(i=-n:n,j=-n:n,KEEP.OUT.ATTRS=FALSE)
+    values$i2j2<-values$i^2+values$j^2
+    values$i<-values$i+(m+1)/2
+    values$j<-values$j+(m+1)/2
+    basis<-split(values,f=values$i2j2)
+    for (i in 1:length(basis)){
+        rownames(basis[[i]])<-NULL
+    }
+    return(basis)
+}
+
+
+#' Get subset of basis functions
+#'
+#' Get a subset of basis functions produced by
+#' \code{getBasisFunctions()}, subsetting based
+#' on distance from center and the number of pixels
+#' in each basis function.
+#'
+#'
+#' @param originalBasis list object produced by
+#' \code{getBasisFunctions()}
+#' @param distFromCenter vector of length 2 indicating
+#' the minimum and maximum distances from the center
+#' @param numPixels integer value indicating the required
+#' number of pixels in each basis function
+#' @return A subset of the original basis with basis
+#' functions of the required distances from the center
+#' and number of pixels.
+#' @examples
+#' basis3 <- getBasisFunctions(3)
+#' basis3_rad1_4pixels <- subsetBasis(basis3,c(1,1),4)
+#'
+
+subsetBasis<-function(originalBasis,distFromCenter,numPixels){
+    if (!missing(distFromCenter)){
+        radii<-unlist(lapply(originalBasis,FUN=function(x) {sqrt(x[1,"i2j2"])}),use.names=FALSE)
+        indicesToKeep<-which(radii>=distFromCenter[1] & radii<=distFromCenter[2])
+        basis<-originalBasis[indicesToKeep]
+    }
+    if (!missing(numPixels)){
+        pixels<-unlist(lapply(basis,FUN=nrow),use.names=FALSE)
+        basis<-basis[which(pixels==numPixels)]
+    }
+    return(basis)
+}
+
+
+
+#' Get statistics for each basis function
+#'
+#' Get statistics on pixel values in each basis function:
+#' mean of the pixel values (basis function coefficient),
+#' residual sum of squares using fitted coefficient,
+#' sum of pixel values.
+#'
+#'
+#' @param ij ij coordinates of the basis function.
+#' Input is typically one item of the list of basis
+#' functions
+#' @param image image that statistics are to be calculated
+#' for
+#' @param requestedStats vector with each element
+#' being a requested statistic, e.g. \code{c("coef","RSS")}.
+#' Possible options are \code{"numPixels"} for the number
+#' of pixels in the input basis function, \code{"coef"} for
+#' the coefficient as described above, \code{"RSS"} for
+#' the residual sum of squares, and \code{"sum"} for
+#' the sum of pixel values.
+#'
+#' @return A list with the requested statistics in the
+#' order provided.
+#' @examples
+#' basis3 <- getBasisFunctions(3)
+#' stats <- statisticsByBasisFunction(basis3[[2]],
+#'          matrix(1:9,nrow=3),c("numPixels","coef"))
+#'
+statisticsByBasisFunction<-function(ij,image,requestedStats){
+    ret<-vector(mode="list",length=length(requestedStats))
+    pixelValues<-image[cbind(ij$i,ij$j)]
+
+    for (i in 1:length(requestedStats)){
+        ret[[i]]<-switch(requestedStats[i],
+                         numPixels=nrow(ij),
+                         coef=mean(pixelValues),
+                         RSS=sum((pixelValues-mean(pixelValues))^2),
+                         sum=sum(pixelValues)
+                         )
+    }
+    return(ret)
+}
+
+#' Fit basis functions
+#'
+#' Fit basis functions to a centered image.
+#'
+#' @param image centered image
+#' @param basis list of basis functions of the appropriate
+#' dimension, such as those
+#' produced by \code{getBasisFunctions()}.
+#'
+#' @return A data frame with the number of pixels in
+#' each basis function, basis function coefficient and
+#' residual sum of squares for each basis function.
+#' @examples
+#' basis3 <- getBasisFunctions(3)
+#' sampleBasis <- fitBasis(matrix(1:9,nrow=3),basis3)
+
+fitBasis<-function(image,basis){
+    imageBasis<-unlist(lapply(basis,function(x) statisticsByBasisFunction(x,image,c("numPixels","coef","RSS"))),use.names=FALSE)
+    imageBasis<-data.frame(matrix(imageBasis,byrow=TRUE,ncol=3))
+    names(imageBasis)<-c("numPixels","coef","RSS")
+    return(imageBasis)
+}
+
---
+++ R/image.R
@@ -0,0 +1,131 @@
+library(raster)
+
+#' Read in a 1944 x 2592 TIFF cartridge image
+#'
+#' Images from the NIST Ballistics and Research Toolmark
+#' Database are in this standard format, with dimensions
+#' 1944 x 2592, and on a 255-grayscale. Images are read
+#' in, returning a matrix with pixel values taking values
+#' 0-255. By default images are cropped to square (1919 x
+#' 1919 pixels) for further processing.
+#'
+#' @param TIFFfilename location of TIFF file to be read in
+#' @param crop a logical value indicating whether the image should
+#' be cropped to 1919 x 1919 pixels
+#' @return A 1919 x 1919 (cropped) or 1944 x 2952 matrix
+#' containing pixel values from 0 to 255.
+#' @examples
+#' LL1_3 <- readCropTIFF(system.file("extdata", "LL1_3.tif",
+#'  package="cartridges"))
+#'
+readCropTIFF<-function(TIFFfilename,crop=TRUE){
+    if (crop==TRUE) {
+        ret<-raster::as.matrix(raster(TIFFfilename))[13:1931,337:2255]
+    } else {
+        ret<-raster::as.matrix(raster(TIFFfilename))
+    }
+    return(ret)
+}
+
+
+#' Shift a 1919 x 1919 image
+#'
+#' @param image image that is to be shifted
+#' @param centeri i-coordinate of desired center
+#' @param centerj j-coordinate of desired center
+#'
+#' @return A shifted 1919 x 1919 image. Pixel values
+#' that are not available are set to 0.
+#'
+#' @examples
+#' shifted <- shiftedImage(LL1_3, 910, 910)
+#'
+shiftedImage<-function(image,centeri,centerj){
+    # default center is at (959,959)
+    # convert desired ij center into dx and dy
+    dy<-centeri-959
+    dx<-959-centerj
+
+    newGrid<-matrix(0,nrow=nrow(image),ncol=ncol(image))
+    if (dx>=0 & dy>=0){
+        newGrid[1:(1919-dy),(1+dx):1919]<-image[(1+dy):1919,1:(1919-dx)]
+    } else if (dx>=0 & dy<0){
+        newGrid[(1-dy):1919,(1+dx):1919]<-image[1:(1919+dy),1:(1919-dx)]
+    } else if (dx<0 & dy>=0) {
+        newGrid[1:(1919-dy),1:(1919+dx)]<-image[(1+dy):1919,(1-dx):1919]
+    } else if (dx<0 & dy<0){
+        newGrid[(1-dy):1919,1:(1919+dx)]<-image[1:(1919+dy),(1-dx):1919]
+    }
+    return(newGrid)
+}
+
+
+#' Get fitted image from basis function coefficients
+#'
+#' @param image centered image
+#' @param basisCoefficients data frame of basis function
+#' coefficients, such as those produced by
+#' \code{fitBasis()}
+#' @param basis list of basis functions of the appropriate
+#' dimension, such as those
+#' produced by \code{getBasisFunctions()}.
+#' @param dimension dimension of image
+#'
+#' @return A matrix of pixel values
+#' @examples
+#' LL1_3_basis <- fitBasis(LL1_3,basis1919)
+#' fittedImage <- getFittedImage(LL1_3_basis,basis1919,1919)
+
+getFittedImage<-function(basisCoefficients,basis,dimension){
+    fittedImage<-matrix(NA,nrow=dimension,ncol=dimension)
+    for (k in 1:length(basis)){
+        ij<-basis[[k]]
+        fittedImage[cbind(ij$i,ij$j)]<-basisCoefficients$coef[k]
+    }
+    return(fittedImage)
+}
+
+library(fields)
+
+
+#' Plot an image
+#'
+#' @param image image to be plotted
+#' @param type either \code{"original"} for images on the
+#' original scale (pixel values 0-255), or \code{"residuals"}
+#' for residual pixel values (-255 to 255).
+#' @param grayscale logical value indicating whether or not
+#' the grayscale is to be plotted.
+#' @param main title for plots with grayscale
+#'
+#' @examples
+#' plotImage(LL1_3,"original",grayscale=FALSE)
+
+
+plotImage<-function(image,type,grayscale,main){
+    dimension<-nrow(image)
+    if (grayscale==FALSE){
+        par(mar=c(0,0,0,0))
+        plot(c(1,dimension), c(1,dimension),type="n",xaxt="n",yaxt="n",xlab = "", ylab = "",yaxs="i",xaxs="i")
+        if (type=="original"){
+            rasterImage(image/255,1,1,dimension,dimension,interpolate=FALSE)
+        } else if (type=="residuals"){
+            rasterImage((image+255)/510,1,1,dimension,dimension,interpolate=FALSE)
+        }
+    } else if (grayscale==TRUE){
+        par(mar=c( 2.1, 2.1, 6.1, 6.1))
+        plot(c(1,dimension), c(1,dimension),type="n",xaxt="n",yaxt="n",xlab = "", ylab = "",yaxs="i",xaxs="i",main=main)
+        if (type=="original"){
+            rasterImage(as.raster(image/255),1,1,dimension,dimension,interpolate=FALSE)
+            image.plot(legend.only=TRUE, zlim=c(0,255),col=gray(0:255/255))
+        } else if (type=="residuals"){
+            rasterImage(as.raster((image+255)/510),1,1,dimension,dimension,interpolate=FALSE)
+            image.plot(legend.only=TRUE, zlim=c(-255,255),col=gray(0:255/255),
+                        axis.args=list(at=seq(-250, 250, 50),
+                                       labels=seq(-250, 250, 50),
+                                       cex.axis=0.8))
+        }
+     }
+}
+
+
